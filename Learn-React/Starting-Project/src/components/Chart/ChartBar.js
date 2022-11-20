1323fde3224d1b416cfb72c84c17b9bffa3b9c09
abhinandjayasankaran <abhinandjayasankaran@gmail.com>
2022-11-20 19:44:26 +0300

    Cleaned up Project File

--- Learn-React/Starting-Project/src/components/Chart/ChartBar.js
+++ Learn-React/Starting-Project/src/components/Chart/ChartBar.js
@@ -6,7 +6,6 @@ function ChartBar(props) {
   
     if(props.maxValue > 0){
        barFillHeight = Math.round((props.value/props.maxValue) * 100) + '%';
-       //barFillHeight = '10%';
     }
 
     return(
