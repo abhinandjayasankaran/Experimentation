b1d2648904fcf87c2be048d29dd06b06677a763d
abhinandjayasankaran <abhinandjayasankaran@gmail.com>
2022-12-10 20:16:36 +0300

    Initial Set up

--- Learn-React/qr-coder/src/App.js
+++ Learn-React/qr-coder/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import qrForm from "./components/qrForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <h1 style={{fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px'}}>QR CODE CREATOR</h1>
+      <div className="container">
+        <qrForm/>
+      </div>
     </div>
   );
 }
---
+++ Learn-React/qr-coder/src/components/qrForm.js
@@ -0,0 +1,11 @@
+function qrForm () {
+    return (
+        <form>
+            <h3>QR Code Generator Form</h3>
+            <label>Input Text:</label>
+            <input type="text" name="inputText" />
+        </form>
+    );
+}
+
+export default qrForm;
